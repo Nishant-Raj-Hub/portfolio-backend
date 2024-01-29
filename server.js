7c17e6230c5889b09af5cdaa53addb5162def226
Nishant-Raj-Hub <nishantraj2109@gmail.com>
2024-01-29 19:46:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const path = require("path");
+// import dotenv from dotenv;
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+const __dirname1 = path.resolve();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// server used to send send emails
+const PORT = process.env.PORT || 5000
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(PORT, () => console.log("Server Running"));
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+   
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send email");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: process.env.EMAIL_USER,
+    subject: "Portfolio - Contact Form Submission",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
